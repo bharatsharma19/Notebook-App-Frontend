4112e303063cc770be449d2b859ad42c29b8ce13
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-20 22:08:27 +0530

    Deletion of Note Completed

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -52,7 +52,19 @@ const NoteState = (props) => {
   };
 
   // Delete a Note
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
+    // API Call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJmNDA0NGJjM2NkNjA3ZGFjNzNlYzRjIn0sImlhdCI6MTY2MDkxMzA1NH0.6LrJRoLuuAM4dkJ5DyPbWoyZ5A4lsEho3OUBPwGyIcU",
+      },
+    });
+    const json = response.json();
+    console.log(json);
+
     const newNotes = notes.filter((note) => {
       return note._id !== id;
     });
