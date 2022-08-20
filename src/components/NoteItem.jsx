be3be5a3ea04a9729b3cf501b16cdd00f5c6f40f
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-20 19:49:50 +0530

    NoteItem Updated

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -6,9 +6,34 @@ const NoteItem = (props) => {
     return (
         <div className="col-md-3">
             <div className="card my-3">
-                <div className="card-body">
-                    <h4 className="card-title">{note.title}</h4>
-                    <h6 className="card-subtitle mb-2 text-muted">{note.tag}</h6>
+            <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        position: "absolute",
+                        right: "0",
+                    }}
+                >
+                    <span className="badge rounded-pill bg-info"> {note.tag} </span>
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        position: "absolute",
+                        left: "0",
+                    }}
+                >
+                    <span className="badge rounded-pill bg-secondary"> {note.date} </span>
+                </div>
+                <div className="card-body my-2">
+                    <div className="d-flex align-items-centerbd-highlight mb-3">
+                        <h4 className="card-title">{note.title}</h4>
+                        <div className="mx-3">
+                            <i className="far fa-trash-alt p-2 bd-highlight" style={{ cursor: "pointer" }}></i>
+                            <i className="far fa-edit p-2 bd-highlight" style={{ cursor: "pointer" }}></i>
+                        </div>
+                    </div>
                     <p className="card-text">
                         {note.desc}
                     </p>
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -10,7 +10,7 @@ const Notes = () => {
         <div className="row my-3">
             <h2 className='text-center'>Your Notes</h2>
             {notes.map((note) => {
-                return <NoteItem note={note} />
+                return <NoteItem key={note._id} note={note} />
             })}
         </div>
     )
