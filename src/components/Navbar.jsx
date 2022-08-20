871891d4039365ffce68b64409903e1b1751bd9f
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-20 18:16:48 +0530

    Code Refactored

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
-    Link
+    Link,
+    useLocation
 } from "react-router-dom";
 
 const Navbar = () => {
+    let location = useLocation();
+    useEffect(() => {
+        console.log(location)
+    }, [location]);
+
     return (
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
