089b0e85082e916b6c3146e0921ae16c66f1fa7f
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-20 18:12:28 +0530

    Cleaned

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,10 @@
 import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
 
 const About = () => {
-    const a = useContext(noteContext)
-
-    useEffect(() => {
-        a.update()
-    }, [])
 
     return (
         <div>
-            <h1>About {a.state.name}</h1>
+            About
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,24 +2,8 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Bharat",
-  };
-
-  const [state, setState] = useState(s1);
-
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Sharma",
-      });
-    }, 1000);
-  };
-
   return (
-    <NoteContext.Provider value={{ state, update }}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   );
 };
 
