7eae5407f55b8a68963734723ace390858c2e61e
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-20 19:21:22 +0530

    Font Awesome Script Added

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -5,7 +5,7 @@ const NoteItem = (props) => {
 
     return (
         <div className="col-md-3">
-            <div className="card">
+            <div className="card my-3">
                 <div className="card-body">
                     <h4 className="card-title">{note.title}</h4>
                     <h6 className="card-subtitle mb-2 text-muted">{note.tag}</h6>
