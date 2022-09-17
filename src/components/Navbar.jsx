3a85c3f1502f97184d63e8fdb335db11c66bf41b
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-09-17 20:54:54 +0530

    Buttons Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,9 +29,9 @@ const Navbar = () => {
                             <a className="nav-link" href="https://www.linkedin.com/in/bharatsharma1909/" target="_blank" rel="noopener noreferrer">Contact Me</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    <form className="d-flex">
+                        <Link className="btn btn-outline-primary mx-2" to="/login" role="button">Sign In</Link>
+                        <Link className="btn btn-outline-primary mx-2" to="/signup" role="button">Sign Up</Link>
                     </form>
                 </div>
             </div>
